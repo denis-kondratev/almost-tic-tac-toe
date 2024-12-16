af079ba5e19d74d6cbd5e1203dfd229fb1dd9ef2
Denis Kondratev <kondratev.da@gmail.com>
2024-12-16 02:54:49 +0100

    Implement game logic and add ml-agents

--- Assets/Scripts/PlayBox.cs
+++ Assets/Scripts/PlayBox.cs
@@ -1,9 +1,106 @@
+using System;
+using System.Net;
+using Unity.MLAgents;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayBox : MonoBehaviour
 {
-    public GameCell[] cells;
-    public Vector2Int playgroundSize;
-    public Player bluePlayer;
-    public Player redPlayer;
+    [SerializeField] private Playground playground;
+    [SerializeField] private PlayerAgent bluePlayer;
+    [SerializeField] private PlayerAgent redPlayer;
+    [SerializeField] private float stepDuration = 2f;
+
+    private Team _turnTeam;
+    private float _nextStep;
+    
+    private void Update()
+    {
+        if (Time.time >= _nextStep)
+        {
+            _nextStep = Time.time + stepDuration;
+            NextStep();
+        }
+    }
+
+    private void NextStep()
+    {
+        switch (playground.State)
+        {
+            case PlaygroundState.None:
+                StartGame();
+                break;
+            case PlaygroundState.Playing:
+                MakeMove();
+                break;
+            case PlaygroundState.BlueWins:
+                OnWin(bluePlayer, redPlayer);
+                break;
+            case PlaygroundState.RedWins:
+                OnWin(redPlayer, bluePlayer);
+                break;
+            case PlaygroundState.BlueMadeInvalidMove:
+                OnInvalidMove(bluePlayer, redPlayer);
+                break;
+            case PlaygroundState.RedMadeInvalidMove:
+                OnInvalidMove(redPlayer, bluePlayer);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private void MakeMove()
+    {
+        if (GetPlayer(_turnTeam).IsWaitingAction)
+        {
+            return;
+        }
+        
+        _turnTeam = _turnTeam switch
+        {
+            Team.Blue => Team.Red,
+            Team.Red => Team.Blue,
+            _ => throw new ArgumentOutOfRangeException(nameof(_turnTeam), _turnTeam, null)
+        };
+        
+        GetPlayer(_turnTeam).MakeMove();
+    }
+
+    private void OnInvalidMove(PlayerAgent penalizedPlayer, PlayerAgent anotherPlayer)
+    {
+        penalizedPlayer.AddReward(-10);
+        penalizedPlayer.EndEpisode();
+        anotherPlayer.EndEpisode();
+        playground.EndGame();
+        Academy.Instance.EnvironmentStep();
+    }
+
+    private void OnWin(PlayerAgent winner, PlayerAgent looser)
+    {
+        winner.AddReward(1);
+        looser.AddReward(-1);
+        winner.EndEpisode();
+        looser.EndEpisode();
+        playground.EndGame();
+        Academy.Instance.EnvironmentStep();
+    }
+
+    private void StartGame()
+    {
+        playground.StartGame();
+        bluePlayer.Reset();
+        redPlayer.Reset();
+        _turnTeam = Random.Range(0, 2) == 0 ? Team.Blue : Team.Red;
+    }
+
+    private PlayerAgent GetPlayer(Team team)
+    {
+        return team switch
+        {
+            Team.Blue => bluePlayer,
+            Team.Red => redPlayer,
+            _ => throw new ArgumentOutOfRangeException(nameof(team), team, null)
+        };
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,65 @@
+using System;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour
-{
-    public Team team;
-    public GamePiece[] pieces;
+{ 
+    [field: SerializeField] public Team Team { get; private set; }
+    [SerializeField] private GamePiece[] pieces;
+    [SerializeField] private Playground playground;
+    
+    private bool[] _hasPiece;
+    
+    public void Reset()
+    {
+        CheckPlayer();
+        Array.ForEach(pieces, x => x.Reset());
+        _hasPiece ??= new bool[pieces.Length];
+        Array.Fill(_hasPiece, true);
+    }
+    
+    public int GetPieceCount()
+    {
+        return pieces.Length;
+    }
+    
+    public bool HasPiece(int pieceSize)
+    {
+        if (pieceSize < 0 || pieceSize > pieces.Length)
+        {
+            Assert.IsTrue(false, $"Invalid piece size. Value: {pieceSize}.");
+            return false;
+        }
+        
+        return _hasPiece[pieceSize];
+    }
+
+    public void MakeMove(int cellIndex, int pieceNumber)
+    {
+        if (pieceNumber < 0 || pieceNumber > pieces.Length)
+        {
+            playground.MakeInvalidMove(Team);
+            return;
+        }
+
+        if (!_hasPiece[pieceNumber])
+        {
+            playground.MakeInvalidMove(Team);
+            return;
+        }
+        
+        _hasPiece[pieceNumber] = false;
+        playground.MakeMove(cellIndex, pieces[pieceNumber]);
+    }
+
+    private void CheckPlayer()
+    {
+        for (var i = 0; i < pieces.Length; i++)
+        {
+            if (pieces[i].Number != i)
+            {
+                throw new Exception($"Invalid piece size: {pieces[i].Number}. Player: {gameObject.name}.");
+            }
+        }
+    }
 }
