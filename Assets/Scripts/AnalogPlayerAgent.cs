6ab43a89151457167ad2a262dcc8ea3fa5f3a900
Denis Kondratev <kondratev.da@gmail.com>
2025-01-03 11:10:08 +0100

    Refactor analog player

--- Assets/Scripts/AnalogPlayerAgent.cs
+++ Assets/Scripts/AnalogPlayerAgent.cs
@@ -6,35 +6,42 @@ using UnityEngine;
 public class AnalogPlayerAgent : PlayerAgent
 {
     private readonly int[] _buffer = new int[Playground.CellCount];
-    
+    private AnalogMoveUtils _moveUtils;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _moveUtils = new AnalogMoveUtils(player, playground);
+    }
+
     public override void CollectObservations(VectorSensor sensor)
     {
         for (var i = 0; i < Playground.CellCount; i++)
         {
             var piece = playground.GetCellPiece(i);
-            
+
             if (piece == null)
             {
                 sensor.AddObservation(0f);
                 continue;
             }
-            
+
             var sign = piece.Team == player.Team ? 1 : -1;
             var amount = (Player.PieceCount - piece.Number) / Player.PieceCount;
             sensor.AddObservation(sign * amount);
         }
-        
+
         for (var i = 0; i < Player.PieceCount; i++)
         {
             sensor.AddObservation(player.HasPiece(i));
         }
-        
+
         for (var i = 0; i < Player.PieceCount; i++)
         {
             sensor.AddObservation(rival.HasPiece(i));
         }
     }
-    
+
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         var actions = actionsOut.ContinuousActions;
@@ -50,63 +57,12 @@ public class AnalogPlayerAgent : PlayerAgent
 
     protected override Move ActionToMove(ActionBuffers actions)
     {
-        var cell = ActionToCell(actions.ContinuousActions[0]);
-        var piece = ActionToPiece(actions.ContinuousActions[1], cell);
-        return new Move(piece, cell);
-    }
-
-    private int ActionToCell(float normalAction)
-    {
-        var minPiece = player.GetMinPiece();
-
-        if (minPiece < 0)
-        {
-            throw new System.InvalidOperationException("Player has no pieces.");
-        }
-        
-        var cellCount = playground.GetAvailableCells(minPiece, _buffer);
-        
-        if (cellCount == 0)
-        {
-            throw new System.InvalidOperationException("Player has no available moves.");
-        }
-        
-        var cellIndex = NormalToIndex(normalAction, cellCount);
-        return _buffer[cellIndex];
-    }
-    
-    private int ActionToPiece(float normalAction, int cell)
-    {
-        var pieceCount = player.GetAvailablePieces(cell, _buffer);
-        
-        if (pieceCount == 0)
-        {
-            throw new System.InvalidOperationException("Player has no available pieces.");
-        }
-        
-        var pieceIndex = NormalToIndex(normalAction, pieceCount);
-        return _buffer[pieceIndex];
+        return _moveUtils.GetMove(actions.ContinuousActions[0], actions.ContinuousActions[1]);
     }
 
     protected override void SetupBrainParameters(BrainParameters parameters)
     {
         parameters.VectorObservationSize = Player.PieceCount * 2 + Playground.CellCount;
         parameters.ActionSpec = ActionSpec.MakeContinuous(2);
-    }
-    
-    private static int NormalToIndex(float normal, int count)
-    {
-        if (normal is < -1 or > 1)
-        {
-            throw new System.ArgumentOutOfRangeException(nameof(normal), normal, $"Value must be in [0, 1]. Actual value is {normal}.");
-        }
-
-        if (count <= 0)
-        {
-            throw new System.ArgumentOutOfRangeException(nameof(count), count, "Value must be positive.");
-        }
-
-        normal = (normal + 1f) / 2f;
-        return normal is 1 ? count - 1 : Mathf.FloorToInt(normal * count);
     }
 }
